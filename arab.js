0284f22c05572c9cfafae76bb5c34adcb1ac1f1a
mac <mac@macs-MacBook-Air.local>
2025-06-14 14:10:31 +0530

    Arab

---
+++ arab.js
@@ -0,0 +1,33 @@
+const tocArabic = {
+  title: "جدول المحتويات",
+  items: [
+    {
+      number: "٠١",
+      title: "معلومات عنا",
+      description: "تعرف على شركتنا وخبراتنا وما يميزنا عن الآخرين.",
+      link: "#about",
+      button: "اقرأ المزيد"
+    },
+    {
+      number: "٠٢",
+      title: "الرؤية والرسالة",
+      description: "اكتشف مبادئنا التوجيهية وأهدافنا طويلة المدى.",
+      link: "#vision-mission",
+      button: "اقرأ المزيد"
+    },
+    {
+      number: "٠٣",
+      title: "خدماتنا",
+      description: "استكشف الخدمات الشاملة التي نقدمها لعملائنا.",
+      link: "#services",
+      button: "اقرأ المزيد"
+    },
+    {
+      number: "٠٤",
+      title: "اتصل بنا",
+      description: "تواصل مع فريقنا لتلبية احتياجاتك في مجال البناء.",
+      link: "#contact",
+      button: "اقرأ المزيد"
+    }
+  ]
+};
